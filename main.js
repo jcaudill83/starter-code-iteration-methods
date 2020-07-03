5820c8ad296a19c1ef7d9df64d984c45e86dd6c6
Justin Caudill <jcaudill_2003@yahoo.com>
2020-07-03 17:49:59 -0400

    project almost done

--- main.js
+++ main.js
@@ -7,3 +7,57 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+//Kata 1*
+
+const activeUser = users.filter(user => user.isActive = "true")
+
+
+printKata(1, users.filter)
+
+//Kata 2
+
+const emailAddress = users.map(user => user.email)
+
+printKata(2, emailAddress)
+
+//Kata 3
+
+const companyProperty = users.some(user => user.company = "OVATION")
+
+printKata(3, companyProperty)
+
+//Kata 4
+
+const userAge = users.find(user =>user.age >= "38")
+
+printKata(4, userAge)
+
+// Kata 5*
+
+const overAge = users.find(user => user.age = "active", "38")
+
+printKata(5, overAge)
+
+// Kata 6*
+// const userBalance = users.
+printKata(6,)
+
+// Kata 7*
+
+printKata(7, )
+
+function printKata (kataNumber, object) {
+    // For the usage of the DETAILS and SUMMARY tags
+    // in HTML, see: http://mdn.io/details-element
+    const detailsElement = document.createElement('details')
+    main.append(detailsElement)
+    //
+    const summaryElement = document.createElement('summary')
+    summaryElement.append("KATA " + kataNumber)
+    detailsElement.append(summaryElement)
+    //
+    // http://mdn.io/json.stringify
+    const stringifiedObject = JSON.stringify(object)
+    detailsElement.append(stringifiedObject)
+}
